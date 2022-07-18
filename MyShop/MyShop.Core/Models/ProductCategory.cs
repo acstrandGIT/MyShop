3b24bc043328eddefe0419bd3c4e007249ae573b
Andrew Strand <astrand@ITAGROUP.com>
2022-07-18 13:04:42 -0500

    Product Grouping

---
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class ProductCategory
+    {
+        public string ProductCategoryId { get; set; }
+        public string ProductCategoryName { get; set; }
+
+        //constructor for this model. So that every time this model is used a new ProductCategory is created. 
+        public ProductCategory()
+        {
+            this.ProductCategoryId = Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepo.cs
@@ -0,0 +1,82 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepo
+    {
+
+        private ObjectCache cache = MemoryCache.Default;
+        private List<ProductCategory> productCategories;
+
+        public ProductCategoryRepo()
+        {
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+        public void Insert(ProductCategory p)
+        {
+            productCategories.Add(p);
+        }
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory productCategoryToUpdate = productCategories.Find(p => p.ProductCategoryId == productCategory.ProductCategoryId);
+
+            if (productCategoryToUpdate != null)
+            {
+                productCategoryToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category Not Found");
+            }
+        }
+
+        public ProductCategory Find(String Id)
+        {
+            ProductCategory productCategory = productCategories.Find(p => p.ProductCategoryId== Id);
+
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category Not Found");
+            }
+
+        }
+
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = productCategories.Find(p => p.ProductCategoryId == Id);
+
+            if (productCategoryToDelete != null)
+            {
+                productCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Category Not Found");
+            }
+        }
+    }
+}
